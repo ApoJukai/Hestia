e0acedc7b4831f24c154cd094fade17ecf7adeb0
ApoJukai <johnharleydeleon@gmail.com>
2026-07-14 11:07:14 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,25 @@
-const CACHE = "hestia-v13-1";
+const CACHE = "hestia-v13-2";
 const ASSETS = [
   "./",
   "./index.html",
-  "./firebase-app.js",
+  "./firebase-app.js?v=13.2",
   "./firebase-config.js",
   "./manifest.webmanifest",
   "./icon.svg"
 ];
-
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
 });
-
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
-      .then(keys => Promise.all(
-        keys.filter(key => key !== CACHE).map(key => caches.delete(key))
-      ))
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
       .then(() => self.clients.claim())
   );
 });
-
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
-
   event.respondWith(
     fetch(event.request)
       .then(response => {
@@ -35,10 +27,6 @@ self.addEventListener("fetch", event => {
         caches.open(CACHE).then(cache => cache.put(event.request, copy));
         return response;
       })
-      .catch(() =>
-        caches.match(event.request).then(cached =>
-          cached || caches.match("./index.html")
-        )
-      )
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
   );
 });
